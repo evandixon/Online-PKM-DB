3bdb169986d7fbfacf693fece3debd1aca09b123
Evan Dixon <evanldixon@live.com>
2016-08-08 13:40:25 -0500

    Updated web config with connection strings and transforms

--- Online-PKM-DB/Models/IdentityModels.vb
+++ Online-PKM-DB/Models/IdentityModels.vb
@@ -19,7 +19,7 @@ End Class
 Public Class ApplicationDbContext
     Inherits IdentityDbContext(Of ApplicationUser)
     Public Sub New()
-        MyBase.New("DefaultConnection", throwIfV1Schema:=False)
+        MyBase.New("UsersDB", throwIfV1Schema:=False)
     End Sub
 
     Public Shared Function Create() As ApplicationDbContext
