cb11f217c62b5522100c3018de43c06265ef634e
Evan Dixon <evanldixon@live.com>
2016-08-08 17:18:34 -0500

    Changed how owin startup class is defined

--- Online-PKM-DB/Startup.vb
+++ Online-PKM-DB/Startup.vb
@@ -1,5 +1,6 @@
-﻿Imports Owin
-
+﻿Imports Microsoft.Owin
+Imports Owin
+<Assembly: OwinStartup(GetType(Online_PKM_DB.Startup))>
 Partial Public Class Startup
     Public Sub Configuration(app As IAppBuilder)
         ConfigureAuth(app)
